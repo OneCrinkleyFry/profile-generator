ea8fe511c13161417aea5252f2ea04ee2b48cb3c
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-10-03 01:05:32 -0600

    added a couple of questions.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+let team = [];
+
+const startInquiry = () => {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: 'name',
+            message: 'Who is the team manager?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is their employee id? (enter a number)'
+        }
+    ])
+        .then(res => {
+            console.log(res);
+        })
+};
+
+startInquiry();
